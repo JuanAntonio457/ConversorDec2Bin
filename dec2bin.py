e880ff1d014f0c02d0e5ee5597d05e4056a7c8d1
JuanAntonio457 <iknowdewey5@gmail.com>
2022-09-15 20:29:15 +0200

    dec2bin.py DEFINITIVE EDITION
    100% NO TRUE 1 LINK TUENTI :)
    derechos a nuestro señor alabado sea Raúl

--- dec2bin.py
+++ dec2bin.py
@@ -7,14 +7,15 @@
 # ------------------------------------------------------
 def dec2bin(numero_decimal, numero_bits):
     numero_binario = bin(numero_decimal)
-	if numero_decimal>=0:
+    if numero_decimal>=0:
         numero_binario = numero_binario[2:len(numero_binario)]  # quita el "0b" del principio
     
         while len(numero_binario) < numero_bits:      # añade 0's a la izquierda si hace falta
             numero_binario = "0" + numero_binario
-	else:
-	    numero_binario= numero_binario[3:len(numero_binario)]
-		while len(numero_binario) < numero_bits:      # añade 0's a la izquierda si hace falta
+    else:
+        numero_binario= numero_binario[3:len(numero_binario)]
+	
+        while len(numero_binario) < numero_bits:
             numero_binario = "1" + numero_binario
     return numero_binario
 
